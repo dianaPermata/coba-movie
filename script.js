f47a850865e35fdd3b0c6b12c4334c837a38c48d
dianaPermata <113364526+dianaPermata@users.noreply.github.com>
2022-10-27 11:55:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,32 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&page=1&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL+'/search/movie?'+API_KEY;
+let button = document.getElementById('btn');
 
-const main = document.getElementById('main');
-const form =  document.getElementById('form');
-const search = document.getElementById('search');
 
-getMovies(API_URL);
+button.addEventListener("click", function(){
 
-function getMovies(url) {
+    let weight = document.getElementById("kg").value;
+    let height = document.getElementById("cm").value;
 
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results)
-        showMovies(data.results);
-    })
-}
+    let BMI;
+    BMI = [weight / (height * height)]*10000;
+    BMI = BMI.toFixed(1);
+    console.log(BMI);
 
-function showMovies(data) {
-    main.innerHTML = '';
+    let status = "";
 
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, release_date} = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-             <img src="${IMG_URL+poster_path}" alt="${title}">
-
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getColor(vote_average)}">${vote_average}</span>
-            </div>
-
-            <div class="movie-date">
-            <h5>${release_date}</h5>
-            </div>
-
-        `
-
-        main.appendChild(movieEl);
-    })
-}
-
-    function getColor(vote) {
-    if(vote>= 8){
-        return 'green'
-    }else if(vote >= 5){
-        return "orange"
-    }else{
-        return 'red'
+    if(BMI < 18.5){
+        status="UnderWeight";
+    } 
+    else if (BMI >= 18.5 && BMI < 25){
+        status="Normal Weight";
+    } 
+     else if (BMI >= 25 && BMI < 30){
+        status="Overweight";
+    } 
+    else if (BMI >= 30){
+        status="obesity";
     }
-}
+    console.log(status);
+    document.getElementById("result").innerHTML = `Your BMI is ${BMI}`
+    document.getElementById("yourBMI").innerHTML=`Which means you are <span>${status}</span>`
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const searchTerm = search.value;
-    if(searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
-    }else{
-        getMovies(API_URL);
-    }
-
-})
-        
+});
